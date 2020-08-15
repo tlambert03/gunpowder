29c15fc9eb0569b6bc7c6ecae129040e91eb8423
Riley White <rileywhite89@gmail.com>
2020-08-15 14:25:37 -0400

    Adds node `RejectArray`
    Provdies the same funtionality for the existing Reject node, but works for Arrays instead of
    graphs..

---
+++ gunpowder/contrib/nodes/reject_array.py
@@ -0,0 +1,51 @@
+from gunpowder.profiling import Timing
+import gunpowder as gp
+import logging
+logger = logging.getLogger(__name__)
+
+
+class RejectArray(gp.BatchFilter):
+    """
+        Rejects array if it's size is 0.
+
+        Args:
+            
+            ensure_nonempty (:class: gp.Array):
+                
+                The array to ensure nonempty.
+    """
+    def __init__(self, ensure_nonempty):
+        self.ensure_nonempty = ensure_nonempty
+
+    def setup(self):
+        self.upstream_provider = self.get_upstream_provider()
+
+    def provide(self, request):
+
+        report_next_timeout = 2
+        num_rejected = 0
+
+        timing = Timing(self)
+        timing.start()
+
+        have_good_batch = False
+        while not have_good_batch:
+
+            batch = self.upstream_provider.request_batch(request)
+
+            if batch.arrays[self.ensure_nonempty].data.size != 0:
+                have_good_batch = True
+                logger.debug(f"Accepted batch with shape: {batch.arrays[self.ensure_nonempty].data.shape}")
+            else:
+                num_rejected += 1
+
+                if timing.elapsed() > report_next_timeout:
+                    logger.info(
+                        f"rejected {report_next_timeout} batches, been waiting for a good one "
+                        "since {report_next_timeout}")
+                    report_next_timeout *= 2
+
+        timing.stop()
+        batch.profiling_stats.add(timing)
+
+        return batch
