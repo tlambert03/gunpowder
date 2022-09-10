9df81914316cb0f7d3501c2c36cd9fedd9f56332
Talley Lambert <talley.lambert@gmail.com>
2022-09-10 19:47:26 -0400

    more types

--- gunpowder/batch.pyi
+++ gunpowder/batch.pyi
@@ -1,16 +1,19 @@
 # flake8: noqa
 from typing import Dict, Iterator, Optional, Tuple, Union, overload
-from .freezable import Freezable
-from .profiling import ProfilingStats
+
+from gunpowder.batch_request import BatchRequest
+from gunpowder.roi import Roi
+
 from .array import Array, ArrayKey
+from .freezable import Freezable
 from .graph import Graph, GraphKey
+from .profiling import ProfilingStats
 
 Key = Union[ArrayKey, GraphKey]
 
 class Batch(Freezable):
     """Contains the requested batch as a collection of :class:`Arrays<Array>`
-    and :class:`Graph` that is passed through the pipeline from sources to
-    sinks.
+    and :class:`Graph` that is passed through the pipeline from sources to sinks.
     """
 
     id: int
@@ -42,90 +45,15 @@ class Batch(Freezable):
     def __getitem__(self, key: ArrayKey) -> Array: ...
     @overload
     def __getitem__(self, key: GraphKey) -> Graph: ...
-    def __len__(self) -> int:
-        ...
-    def __contains__(self, key: Key) -> bool:
-        ...
-    def __delitem__(self, key: Key) -> None:
-        ...
+    def __len__(self) -> int: ...
+    def __contains__(self, key: Key) -> bool: ...
+    def __delitem__(self, key: Key) -> None: ...
     def items(self) -> Iterator[Tuple[Key, Union[Array, Graph]]]:
         """Provides a generator iterating over key/value pairs."""
     def get_total_roi(self) -> Optional[Roi]:
         """Get the union of all the array ROIs in the batch."""
-        
-
-        total_roi = None
-
-        for _, array in self.arrays.items():
-            if not array.spec.nonspatial:
-                if total_roi is None:
-                    total_roi = array.spec.roi
-                else:
-                    total_roi = total_roi.union(array.spec.roi)
-
-        for _, graph in self.graphs.items():
-            if total_roi is None:
-                total_roi = graph.spec.roi
-            else:
-                total_roi = total_roi.union(graph.spec.roi)
-
-        return total_roi
-    def __repr__(self):
-
-        r = "\n"
-        for collection_type in [self.arrays, self.graphs]:
-            for (key, obj) in collection_type.items():
-                r += "\t%s: %s\n" % (key, obj.spec)
-        return r
-    def crop(self, request, copy=False):
+    def __repr__(self) -> str: ...
+    def crop(self, request: BatchRequest, copy: bool = False) -> Batch:
         """Crop batch to meet the given request."""
-
-        cropped = Batch()
-        cropped.profiling_stats = self.profiling_stats
-        cropped.loss = self.loss
-        cropped.iteration = self.iteration
-
-        for key, val in request.items():
-            assert key in self, "%s not contained in this batch" % key
-            if val.roi is None:
-                cropped[key] = self[key]
-            else:
-                if isinstance(key, GraphKey):
-                    cropped[key] = self[key].crop(val.roi)
-                else:
-                    cropped[key] = self[key].crop(val.roi, copy)
-
-        return cropped
-    def merge(self, batch, merge_profiling_stats=True):
-        """Merge this batch (``a``) with another batch (``b``).
-
-        This creates a new batch ``c`` containing arrays and graphs from
-        both batches ``a`` and ``b``:
-
-            * Arrays or Graphs that exist in either ``a`` or ``b`` will be
-              referenced in ``c`` (not copied).
-
-            * Arrays or Graphs that exist in both batches will keep only
-              a reference to the version in ``b`` in ``c``.
-
-        All other cases will lead to an exception.
-        """
-
-        merged = shallow_copy(self)
-
-        for key, val in batch.items():
-            # TODO: What is the goal of `val.spec.roi is None`? Why should that
-            # mean that the key in merged gets overwritten?
-            if key not in merged or val.spec.roi is None:
-                merged[key] = val
-            elif key in merged:
-                merged[key] = val
-
-        if merge_profiling_stats:
-            merged.profiling_stats.merge_with(batch.profiling_stats)
-        if batch.loss is not None:
-            merged.loss = batch.loss
-        if batch.iteration is not None:
-            merged.iteration = batch.iteration
-
-        return merged
+    def merge(self, batch: Batch, merge_profiling_stats: bool = True) -> Batch:
+        """Merge this batch (``a``) with another batch (``b``)."""
